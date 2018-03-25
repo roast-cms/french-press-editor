11ecbe744aa3e5f23c02f780252e79f4c5f20853
Dmitri <d@eurekaking.com>
2018-03-25 13:26:37 +0700

    Fix spelling, concise msg

--- src/utils/image-rules.js
+++ src/utils/image-rules.js
@@ -13,7 +13,7 @@ export const forceImageRestrictions = (size, type, max = 10) => {
   return new Promise((resolve, reject) => {
     if (size / 1000000 <= max && correctFileType) resolve()
     else {
-      let message = size / 1000000 <= max ? "oversize" : "wront_type"
+      let message = size / 1000000 <= max ? "size" : "mime"
       reject(message)
     }
   })
