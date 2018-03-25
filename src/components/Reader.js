abb5a5c631b514dc6b5c29113ca3797757d81ab5
Dmitri <d@eurekaking.com>
2018-03-25 17:25:30 +0700

    Fix missing return statement

--- src/components/Reader.js
+++ src/components/Reader.js
@@ -11,24 +11,26 @@ import { renderNode, renderMark } from "../render"
 //
 // component export
 export const Reader = props => {
-  <Editor
-    // this is the Slate editor component that renders Slate JSON value object
-    //
-    // props defineable by user
-    options={{
-      ...props.options,
-      imagePlaceholder:
-        (props.options && props.options.imagePlaceholder) ||
-        "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
-      domain: (props.options && props.options.domain) || ""
-    }}
-    components={props.components}
-    value={Value.fromJSON(props.value)}
-    //
-    // props inferred from Editor architecture
-    readOnly={true}
-    schema={schema}
-    renderNode={renderNode}
-    renderMark={renderMark}
-  />
+  return (
+    <Editor
+      // this is the Slate editor component that renders Slate JSON value object
+      //
+      // props defineable by user
+      options={{
+        ...props.options,
+        imagePlaceholder:
+          (props.options && props.options.imagePlaceholder) ||
+          "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+        domain: (props.options && props.options.domain) || ""
+      }}
+      components={props.components}
+      value={Value.fromJSON(props.value)}
+      //
+      // props inferred from Editor architecture
+      readOnly
+      schema={schema}
+      renderNode={renderNode}
+      renderMark={renderMark}
+    />
+  )
 }
