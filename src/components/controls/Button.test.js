defef80a0f71e31e01fc732c767a621f185ac887
Dmitri <d@eurekaking.com>
2018-06-27 00:03:22 +0700

    Set up jest

--- src/components/controls/Button.test.js
+++ src/components/controls/Button.test.js
@@ -3,10 +3,6 @@ import React from "react";
 import { shallow } from "enzyme";
 
 import { TinyButton } from "./Button";
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-configure({ adapter: new Adapter() });
 
 it("Render TinyButton without crashing", () => {
   shallow(<TinyButton />);
