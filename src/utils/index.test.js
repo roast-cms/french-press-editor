174f041056eea1b70085c811ea0a195d3d45cb38
Dmitri <d@eurekaking.com>
2018-08-01 01:05:05 -0700

    Format

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -15,9 +15,9 @@ import {
 // } from "./actions-storage";
 
 test("Reject images over 10mb via forceImageRestrictions()", () => {
-  return expect(
-    forceImageRestrictions(10000001, "image/jpeg")
-  ).rejects.toEqual("size");
+  return expect(forceImageRestrictions(10000001, "image/jpeg")).rejects.toEqual(
+    "size"
+  );
 });
 test("Reject images over custom size via forceImageRestrictions()", () => {
   return expect(
@@ -25,7 +25,7 @@ test("Reject images over custom size via forceImageRestrictions()", () => {
   ).rejects.toEqual("size");
 });
 test("Reject GIF (unsupported) image mime via forceImageRestrictions()", () => {
-  return expect(
-    forceImageRestrictions(10000000, "image/gif")
-  ).rejects.toEqual("mime");
+  return expect(forceImageRestrictions(10000000, "image/gif")).rejects.toEqual(
+    "mime"
+  );
 });
