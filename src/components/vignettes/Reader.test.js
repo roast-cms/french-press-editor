73047f1dc7251b7fb3f94493a11980aef4da2f8c
Dmitri <d@eurekaking.com>
2018-09-03 15:10:15 -0700

    Remove unconsumed  prop, add comments and acknowledgements

--- src/components/vignettes/Reader.test.js
+++ src/components/vignettes/Reader.test.js
@@ -4,8 +4,15 @@ import React from "react"
 
 import {shallow} from "enzyme"
 
-import Reader, {addRootSerialNumbers, rules, nodeFactory} from "./Reader"
 import {EXAMPLE_VALUE} from "../../../examples/constants"
+import Reader, {
+  addKey,
+  addRootSerialNumbers,
+  nodeFactory,
+  rules,
+  serializeLeaf,
+  serializeString,
+} from "./Reader"
 
 test("rules() with no props creates three serialize objects in an array", () => {
   expect(rules({})).toHaveLength(3)
@@ -41,6 +48,33 @@ test("addRootSerialNumbers adds serial numbers to (shallow) node objects", () =>
 test("nodeFactory maps Value nodes onto serializeNode function instances, produces six blocks", () => {
   const serializeNode = jest.fn()
   expect(nodeFactory({value: EXAMPLE_VALUE})).toHaveLength(6)
+})
+
+test("serializeLeaf maps leaves into marks components with keys", () => {
+  expect(
+    serializeLeaf({
+      object: "leaf",
+      text: "This is the first post to go onto this website.",
+      marks: [
+        {
+          data: {},
+          object: "mark",
+          type: "italic",
+        },
+      ],
+    })
+  ).toMatchSnapshot()
+})
+
+test("serializeString returns string, wrapped in consistent components.", () => {
+  expect(
+    serializeString({text: "This is the first post to go onto this website."})
+  ).toMatchSnapshot()
+})
+
+test("addKey creates React elements that match snapshot.", () => {
+  const element = shallow(<span node={{}}>Element</span>)
+  expect(addKey(element)).toMatchSnapshot()
 })
 
 test("Render Reader without crashing, matches snapshot", () => {
