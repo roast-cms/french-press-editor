84ce4e26778c5a5302e33b0a33592c76d7d044de
Dmitri <d@eurekaking.com>
2018-09-29 11:40:03 -0700

    Restored paste plain html function on plugin

--- src/plugins/paste-html.js
+++ src/plugins/paste-html.js
@@ -12,6 +12,7 @@ export const Paste = options => {
     onPaste(event, change) {
       const transfer = getEventTransfer(event)
       if (transfer.type !== "html") return
+      if (transfer.isShift) return
       const {document} = html.deserialize(transfer.html)
       change.insertFragment(document)
       return true
