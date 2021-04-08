8f9414f057493952a6757be181877ff4ba588d80
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2021-04-07 20:17:20 -0700

    Upgraded webpack

--- README.md
+++ README.md
@@ -215,7 +215,7 @@ To learn more about what you can customize and build for `french-press-editor` t
 
 If you have a feature request, bug report or a question, please submit an issue. **Please keep in mind that this tool isn't built for everyone and it works best when there are _less_ features cluttering the experience and weighing down the package.**
 
-**Pull requests are very much welcome.** To get started with the code: clone the repo, run `yarn install` then `yarn start` and open up `http://localhost:3002` in your browser.
+**Pull requests are very much welcome.** To get started with the code: clone the repo, run `yarn install` then `yarn dev` and open up `http://localhost:3002` in your browser.
 
 **Every folder** in this repo has a `README.md` file to help you along.
 
--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,12 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          presets: ["@babel/preset-react", "@babel/preset-env"],
-          plugins: ["@babel/plugin-proposal-class-properties"],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
         },
       },
       {
@@ -21,19 +23,22 @@ module.exports = {
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, //to support @font-face rule
-        loader: "url-loader",
-        query: {
-          limit: "10000",
-          name: "[name].[ext]",
-          outputPath: "fonts/",
-          //the fonts will be emmited to public/assets/fonts/ folder
-          //the fonts will be put in the DOM <style> tag as eg. @font-face{ src:url(assets/fonts/font.ttf); }
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: "10000",
+            name: "[name].[ext]",
+            outputPath: "fonts/",
+            //the fonts will be emmited to public/assets/fonts/ folder
+            //the fonts will be put in the DOM <style> tag as eg. @font-face{ src:url(assets/fonts/font.ttf); }
+          },
         },
       },
     ],
   },
   resolve: {
     extensions: ["*", ".js"],
+    fallback: {path: require.resolve("path-browserify")},
   },
   devServer: {
     disableHostCheck: true,
